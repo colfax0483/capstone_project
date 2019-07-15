fb5298a7a9de5255e976e2dcd0a731430535545b
gkdldtka <gkdldtka@naver.com>
2019-07-15 16:54:33 +0900

    한글Registry 클래스화
    한글2010, 한글2015 레지스트리 출력, 클래스화

--- Registry.py
+++ Registry.py
@@ -1,12 +1,47 @@
 import winreg
 
 
-#레지스트리 주소
-key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\HNC\Hwp\8.0\HwpFrame\RecentFile")
-#서브주소가 바뀌면 바꿔줘야됨
-for registry in range(len("Software\HNC\Hwp\8.0\HwpFrame\RecentFile")):#레지스트리 주소에서 뽑아오는 함수
-    try:
-        registry = winreg.EnumValue(key, registry)
-        print(registry)
-    except:
-        break
+# 한글 레지스트리
+
+
+class Hwp:
+
+    def __init__(self):
+        self.hwp10 = {}
+        self.hwp15 = {}
+
+    def hwp2010(self): # 한글2010
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\HNC\Hwp\8.0\HwpFrame\RecentFile") #한글 2010경로
+        for registry in range(0, 10):  # 0에서 10까지 반복
+            registrykey = winreg.EnumValue(key, registry)
+            if registrykey[0].startswith('file'):
+                print(registrykey[1].decode('utf-16')[:-5])
+        return self.hwp10
+
+    def hwp2015(self): # 한글2015
+        key2 = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\Hnc\Hwp\9.0\HwpFrame_KOR\RecentFile") #한글 2015경로
+        for registry2 in range(0, 10):
+            registrykey2 = winreg.EnumValue(key2, registry2)
+            if registrykey2[0].startswith('file'):
+                print(registrykey2[1].decode('utf-16')[:-5])
+        return self.hwp15
+
+
+def main():
+    print("어떤 항목을 출력할까요?\n1. 한글2010 2. 한글2015")
+    select = int(input("숫자를 입력하세요: "))
+
+    hwpregistry = Hwp() # 클래스 호출
+
+    if select == 1:
+        hwpregistry.hwp2010()# 클래스안에 함수호출
+
+    elif select == 2:
+        hwpregistry.hwp2015()
+
+    else:
+        print("잘못 선택 하셨습니다")
+
+
+if __name__ == '__main__':
+    main()
