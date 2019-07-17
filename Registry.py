22a26f0606d55d0f1fba2ca797a1642e11aa4eda
gkdldtka <gkdldtka@naver.com>
2019-07-17 10:16:56 +0900

    한글, 오피스 레지스트리 출력
    한글, 오피스 레지스트리 출력 클래스화 에러처리

--- Registry.py
+++ Registry.py
@@ -3,10 +3,10 @@ import os
 
 # 한글 레지스트리
 
-class Hwp:
+class Hwpoffice:
 
     def __init__(self):
-        self.reshwp = []
+        self.reg = []
 
     def hwp2010(self): # 한글2010
         try:
@@ -16,8 +16,8 @@ class Hwp:
         for registry in range(0, 10):  # 0에서 10까지 반복
             registrykey = winreg.EnumValue(key, registry)
             if registrykey[0].startswith('file'):
-                self.reshwp.append(os.path.basename(registrykey[1].decode('utf-16').encode('utf-8').decode('utf-8')[:-5])) #디코딩 후 불필요한 문자열 제거
-        return self.reshwp
+                self.reg.append(os.path.basename(registrykey[1].decode('utf-16').encode('utf-8').decode('utf-8')[:-5])) #디코딩 후 불필요한 문자열 제거
+        return self.reg
 
     def hwp2015(self): # 한글2015
         try:
@@ -27,13 +27,58 @@ class Hwp:
         for registry2 in range(0, 10):
             registrykey2 = winreg.EnumValue(key2, registry2)
             if registrykey2[0].startswith('file'):
-                self.reshwp.append(os.path.basename(registrykey2[1].decode('utf-16').encode('utf-8').decode('utf-8')[:-5]))
-        return self.reshwp
+                self.reg.append(os.path.basename(registrykey2[1].decode('utf-16').encode('utf-8').decode('utf-8')[:-5]))
+        return self.reg
+
+    def word2016(self): # 워드2016
+        try:
+            key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Office\\16.0\\Word\\File MRU") #워드2016경로
+        except FileNotFoundError: #FileNotFoundError뜨면 넘어가기
+            return
+        try:
+            for registry in range(0, 10):  # 0에서 10까지 반복
+                registrykey = winreg.EnumValue(key, registry)
+                if registrykey[0]:
+                    self.reg.append(os.path.basename(registrykey[1][:-5])) #디코딩 후 불필요한 문자열 제거
+            return self.reg
+        except OSError:
+            return
+
+    def excel2016(self): # 엑셀2016
+        try:
+            key2 = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Office\\16.0\\Excel\\File MRU") #엑셀2016경로
+        except FileNotFoundError:
+            return
+        try:
+            for registry2 in range(0, 10):
+                registrykey2 = winreg.EnumValue(key2, registry2)
+                if registrykey2[0]:
+                    self.reg.append(os.path.basename(registrykey2[1][:-5]))
+            return self.reg
+        except OSError:
+            return
+
+    def point2016(self): # 파워포인트2016
+        try:
+            key3 = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Office\\16.0\\PowerPoint\\File MRU") #파워포인트2016 경로
+        except FileNotFoundError:
+            return
+        try:
+            for registry3 in range(0, 10):
+                registrykey3 = winreg.EnumValue(key3, registry3)
+                if registrykey3[0]:
+                    self.reg.append(os.path.basename(registrykey3[1][:-5]))
+            return self.reg
+        except OSError:
+            return
 
     def selecter(self):
         self.hwp2010()
         self.hwp2015()
-        return self.reshwp
+        self.word2016()
+        self.excel2016()
+        self.point2016()
+        return self.reg
 
 
 def main():
@@ -54,9 +99,9 @@ def main():
             print("잘못 선택 하셨습니다")
     '''
 
-    hwpregistry = Hwp()
-    reghwp = hwpregistry.selecter()
-    print(reghwp)
+    stry = Hwpoffice()
+    regis = stry.selecter()
+    print(regis)
 
 if __name__ == '__main__':
     main()
