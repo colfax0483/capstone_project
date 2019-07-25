23461022ed18f2aaba786757687326c958895f13
rs0731 <49332306+rs0731@users.noreply.github.com>
2019-07-25 10:20:57 +0900

    Update ChromeHistory.py
    파이어폭스 시간 구현

--- ChromeHistory.py
+++ ChromeHistory.py
@@ -93,34 +93,43 @@ class Chrome(Mysql):
 class Firefox(Mysql):
     def __init__(self, historyfile = None): #변수에 아무 값도 입력되지 않았을때, 어떤 값으로 초기화할지 지정해 줄 수 있다.
         super(__class__, self).__init__(historyfile)
-        self.filename = []
-        self.titlename = []
-        self.selecter = 0 # 전체 or 다운로드파일 or 방문 기록 선택
-        self.historyfile = historyfile or 'C:\\Users\\Jang\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\0q5fx2ae.default-release\\places.sqlite'
-        #self.firefox = "C:\Users\Jang\AppData\Roaming\Mozilla\Firefox\Profiles\<Random>.default-release\places.sqlite"
+        self.title = {}
+        self.file = {}
+        self.selecter = 0  # 전체 or 다운로드파일 or 방문 기록 선택
+        self.firefox = firefox or 'C:\\Users\\Jang\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\kuaxamep.default-release\\places.sqlite'
+        # self.firefox = "C:\Users\Jang\AppData\Roaming\Mozilla\Firefox\Profiles\<Random>.default-release\places.sqlite"
         # <Random> 값이 존재하는데, 이 값은 Firefox 경로 밑에 profile.ini 파일 아래에 존재
 
-    def webtitle(self, con): #방문한 웹사이트명
-        self.select1 = 'SELECT url, title FROM moz_places;' # 방문 사이트
+    def datetime(self, timestamp):  # 시간 변환
+        timestamp = timestamp
+        visit_time = datetime.datetime(1970, 1, 1, 9) + datetime.timedelta(microseconds=int(timestamp))
+        return visit_time
+
+    def webtitle(self, con):  # 방문한 웹사이트명
+        self.select1 = 'SELECT moz_places.url, moz_places.title, moz_historyvisits.visit_date FROM moz_places, moz_historyvisits WHERE moz_places.id = moz_historyvisits.place_id;'  # 방문 사이트
 
         for row in con.execute(self.select1):  # 방문한 사이트 타이틀
-            if row[1] != None:  # 내용이 없는것 제외
-                self.titlename.append(row[1])
+            titledate = str(self.datetime(row[2]))
+            visit = row[1]
 
-        print(self.titlename)
+            if visit != None:  # 내용이 없는것 제외
+                self.title[visit] = titledate
 
-    def downloadfile(self, con): #다운로드 파일명
-        self.select2 = 'SELECT anno_attribute_id, content FROM moz_annos;' # 다운로드 받은 파일
+        print(self.title)
+
+    def downloadfile(self, con):  # 다운로드 파일명
+        self.select2 = 'SELECT anno_attribute_id, content, dateAdded FROM moz_annos;'  # 다운로드 받은 파일
 
         for row in con.execute(self.select2):  # 다운로드 받은 파일
-            if str(row[0]) != '2':  # 다운로드 url을 갖고 오기위한 moz_places의 place식별자
-                # file url 정보와 나머지 정보를 나누어 저장
-                f = os.path.basename(row[1])  # 다운로드 받은 파일 추출
-                # name = os.path.splitext(f)[0]  # 파일명과 확장자 분리 후, 파일명만 저장
-                if f != '':  # 내용이 없는것 제외
-                    self.filename.append(f)
+            downdate = str(self.datetime(row[2]))
 
-        print(self.filename)
+            if str(row[0]) != '2':  # 다운로드 url을 갖고 오기위한 moz_places의 place식별자. file url 정보와 나머지 정보를 나누어 저장
+                down = os.path.basename(row[1])  # 다운로드 받은 파일 추출
+                # name = os.path.splitext(down)[0]  # 파일명과 확장자 분리 후, 파일명만 저장
+                if down != None:  # 내용이 없는것 제외
+                    self.file[down] = downdate
+
+        print(self.file)
 
 def main():
     print("어떤 항목을 출력할까요?\n0. 전부 분석 1. 방문기록 2. 다운로드 파일")
